0d3a4d0e4be16afe5c14428fadbf7b3172a40514
deyyar <69407559+deyyar@users.noreply.github.com>
2020-08-10 00:52:44 +0300

    Add files via upload

--- userbot/plugins/speedtest.py
+++ userbot/plugins/speedtest.py
@@ -5,23 +5,23 @@ from telethon import events
 from datetime import datetime
 import io
 import speedtest
-from userbot.utils import admin_cmd
+from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("speedtest ?(.*)"))
+@borg.on(admin_cmd(pattern="speedtest ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    as_text = True
-    as_document = False
+    as_text = False
+    as_document = True
     if input_str == "image":
         as_document = False
     elif input_str == "file":
         as_document = True
     elif input_str == "text":
         as_text = True
-    await event.edit("`Calculating my internet speed. Please wait!`")
+    await event.edit("Calculating my internet speed. Please wait!")
     start = datetime.now()
     s = speedtest.Speedtest()
     s.get_best_server()
@@ -43,13 +43,12 @@ async def _(event):
         response = s.results.share()
         speedtest_image = response
         if as_text:
-            await event.edit("""`SpeedTest completed in {} seconds`
-
-`Download: {}`
-`Upload: {}`
-`Ping: {}`
-`Internet Service Provider: {}`
-`ISP Rating: {}`""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
+            await event.edit("""**SpeedTest** completed in {} seconds
+Download: {}
+Upload: {}
+Ping: {}
+Internet Service Provider: {}
+ISP Rating: {}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
         else:
             await borg.send_file(
                 event.chat_id,

12b8ad2bd28b33079af98b0d706315b0796d7c82
deyyar <69407559+deyyar@users.noreply.github.com>
2020-08-19 02:43:58 +0300

    Add files via upload

--- userbot/plugins/sca.py
+++ userbot/plugins/sca.py
@@ -1,23 +1,12 @@
 """Send Chat Actions
-Syntax: .scha <option> <time in sec>
-        scha options: Options for sca 
-
-typing
-contact
-game
-location
-voice
-round
-video
-photo
-document
-cancel"""
+Syntax: .sca <option>
+Options: typing, contact, game, location, voice, round, video, photo, document, cancel"""
 
 import asyncio
 from uniborg.util import admin_cmd
- 
- 
-@borg.on(admin_cmd("scha ?(.*)"))
+
+
+@borg.on(admin_cmd(pattern="sca ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -27,4 +16,4 @@ async def _(event):
     if input_str:
         action = input_str
     async with borg.action(event.chat_id, action):
-        await asyncio.sleep(86400)  # type for 10 seconds
+        await asyncio.sleep(10)  # type for 10 seconds

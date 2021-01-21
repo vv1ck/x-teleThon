a533f48e46e838bc96b9cf2c347bff8d24f6d2d4
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-01-21 21:17:51 +0300

    Update Kill.py

--- userbot/plugins/Kill.py
+++ userbot/plugins/Kill.py
@@ -1,4 +1,4 @@
-"""COMMAND : .kill"""
+"""COMMAND : .قتل"""
 
 from telethon import events
 
@@ -23,7 +23,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "kill":
+    if input_str == "قتل":
 
         await event.edit(input_str)
 

0270539fe65c15bd7100a5909913dee485beed68
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-01-24 02:47:03 +0300

    Update Earth.py

--- userbot/plugins/Earth.py
+++ userbot/plugins/Earth.py
@@ -1,16 +1,16 @@
 # (c) @UniBorg
-# Original written by @UniBorg edit by @I_m_Rock
+# Original written by @UniBorg edit by @vv1ck
 
 from telethon import events
 import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.earth", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.fuk2", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+	deq = deque(list("🖕🖕🏻🖕🏼🖕🏽🖕🏾🖕🏿"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))

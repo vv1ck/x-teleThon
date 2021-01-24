0f7b590a790dec1b6c6387fad2b84d4f668e68aa
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-01-24 05:26:53 +0300

    Update plane.py

--- userbot/plugins/plane.py
+++ userbot/plugins/plane.py
@@ -3,28 +3,50 @@ from telethon import events
 import asyncio
 import os
 import sys
+import time
 
-
-@borg.on(events.NewMessage(pattern=r"\.plane", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.plin", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
         
         
-    await event.edit("✈-------------")
-    await event.edit("-✈------------")
-    await event.edit("--✈-----------")
-    await event.edit("---✈----------")
-    await event.edit("----✈---------")
-    await event.edit("-----✈--------")
-    await event.edit("------✈-------")
-    await event.edit("-------✈------")
-    await event.edit("--------✈-----") 
-    await event.edit("---------✈----")
-    await event.edit("----------✈---")
-    await event.edit("-----------✈--")
-    await event.edit("------------✈-")
-    await event.edit("-------------✈")
-    await asyncio.sleep(3)
-    await event.delete()
+    await event.edit("✈------------------")
+    time.sleep(0.2)
+    await event.edit("-✈-----------------")
+    time.sleep(0.2)
+    await event.edit("--✈----------------")
+    time.sleep(0.2)
+    await event.edit("---✈---------------")
+    time.sleep(0.2)
+    await event.edit("----✈--------------")
+    time.sleep(0.2)
+    await event.edit("-----✈-------------")
+    time.sleep(0.2)
+    await event.edit("------✈------------")
+    time.sleep(0.2)
+    await event.edit("-------✈-----------")
+    time.sleep(0.2)
+    await event.edit("--------✈----------") 
+    time.sleep(0.2)
+    await event.edit("---------✈---------")
+    time.sleep(0.2)
+    await event.edit("----------✈--------")
+    time.sleep(0.2)
+    await event.edit("-----------✈-------")
+    time.sleep(0.2)
+    await event.edit("------------✈------")
+    time.sleep(0.2)
+    await event.edit("-------------✈-----")
+    time.sleep(0.2)
+    await event.edit("--------------✈----")
+    time.sleep(0.2)
+    await event.edit("---------------✈---")
+    time.sleep(0.2)
+    await event.edit("----------------✈--")
+    time.sleep(0.2)
+    await event.edit("-----------------✈-")
+    time.sleep(0.2)
+    await event.edit("------------------✈@vv1ck")
+    await asyncio.sleep(5)
 
